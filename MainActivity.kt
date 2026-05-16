785421b07aedd73a3ae9fa8f65d4e3626dfd88b2
dcheshirecat <danieldinadin@gmail.com>
2026-05-16 18:06:21 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,10 +1,8 @@
 package com.runehatch
-
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String = "runehatch"
     override fun createReactActivityDelegate(): ReactActivityDelegate =
